e0a88eb976f7f3841c9457fa18a904470fcd9a23
zarate <232310065@itslerdo.edu.mx>
2025-11-09 16:55:39 -0600

    Creacion de la API con usuarios

--- BACK/BACK/Program.cs
+++ BACK/BACK/Program.cs
@@ -1,52 +1,45 @@
-using DorjaData;
+﻿using DorjaData;
 using DorjaModelado.Repositories;
-using MySql.Data.MySqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-// Politica del CORS
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-                      policy =>
-                      {
-                          policy.WithOrigins("http://localhost:5173") // URL del frontend
-                                .AllowAnyHeader()
-                                .AllowAnyMethod();
-                      });
+    options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
 });
 
-// Agrega servicios necesarios para Web API
 builder.Services.AddControllers();
-builder.Services.AddOpenApi(); //Swagger
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-//var mySQLConfiguration = new MySQLConfiguration(builder.Configuration.GetConnectionString("DorjaConnection"));
-//builder.Services.AddSingleton(mySQLConfiguration);
-
-builder.Services.AddSingleton(new MySqlConnection(builder.Configuration.GetConnectionString("DorjaConnection")));
-
-builder.Services.AddScoped<IUserRepository, IUserRepository>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 
+// ✅ REGISTRAR CONFIGURACIÓN MYSQL
+builder.Services.AddSingleton(new MySQLConfiguration(
+    builder.Configuration.GetConnectionString("DorjaConnection")
+));
 
+
+// REGISTRAR REPOSITORIO
+builder.Services.AddScoped<IUserRepository, UsersRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseCors(MyAllowSpecificOrigins);
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
