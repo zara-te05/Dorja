bed71b8a571de3e8b6ea1ad45560dd7e8efc546f
zarate <232310065@itslerdo.edu.mx>
2025-11-13 10:47:23 -0600

    Implementacion de API en JS para el login y signup asi como en los controllers

--- PROYECT/FRONT/wwwroot/api.js
+++ PROYECT/FRONT/wwwroot/api.js
@@ -1,6 +1,6 @@
 ﻿// @ts-ignore
 window.api = {
-    baseUrl: 'https://api.example.com',
+     baseUrl: 'https://localhost:7107/api/users',
 
     signup: async (data) => {
         try {
@@ -20,6 +20,34 @@ window.api = {
             return { success: true, data: result.message || "Usuario creado correctamente" };
         } catch (error) {
             console.error('Error en signup API:', error);
+            return { success: false, message: error.message };
+        }
+    },
+
+    // Login 
+    login: async (data) => {
+    
+        try{
+
+            //@ts-ignore
+            const response = await fetch(`${window.api.baseUrl}/login`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(data)
+            });
+
+            const result = await response.json().catch(() => ({}));
+
+            if (!response.ok) {
+            
+                throw new Error(result.message || 'Credenciales incorrectas');
+            }
+
+            return { success: true, data: result };
+
+
+        } catch(error){
+            console.error('Error en login API:', error);
             return { success: false, message: error.message };
         }
     }
