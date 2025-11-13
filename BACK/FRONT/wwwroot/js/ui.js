5b633d7926cfbadfea141adaa4054aa664ce329f
zarate <232310065@itslerdo.edu.mx>
2025-11-13 09:31:49 -0600

    Actualización completa desde carpeta PROYECT

--- BACK/FRONT/wwwroot/js/ui.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('mousemove', (e) => {
-    const root = document.documentElement;
-    root.style.setProperty('--mouse-x', e.clientX + 'px');
-    root.style.setProperty('--mouse-y', e.clientY + 'px');
-});
